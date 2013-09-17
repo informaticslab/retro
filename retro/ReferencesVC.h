bc4c31d993d62437b2f9b4c3425340ba464ecad0
Greg Ledbetter <gsledbetter@gmail.com>
2013-09-17 10:24:09 -0400

    Add References bar button to toolbar and associated View Controller.

---
+++ retro/ReferencesVC.h
@@ -0,0 +1,13 @@
+//
+//  ReferencesVC.h
+//  retro
+//
+//  Created by jtq6 on 9/17/13.
+//  Copyright (c) 2013 jtq6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReferencesVC : UIViewController
+
+@end
---
+++ retro/ReferencesVC.m
@@ -0,0 +1,38 @@
+//
+//  ReferencesVC.m
+//  retro
+//
+//  Created by jtq6 on 9/17/13.
+//  Copyright (c) 2013 jtq6. All rights reserved.
+//
+
+#import "ReferencesVC.h"
+
+@interface ReferencesVC ()
+
+@end
+
+@implementation ReferencesVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
